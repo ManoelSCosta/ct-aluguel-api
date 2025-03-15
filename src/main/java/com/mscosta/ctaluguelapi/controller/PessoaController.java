1b4a188327dcd9309d8a795c3274de28cfa6ec41
Manoel Costa <costamanoelsc@icloud.com>
2025-03-15 16:40:55 -0300

    Feat: adição de serviço de buscar um pessoa pelo id.

--- src/main/java/com/mscosta/ctaluguelapi/controller/PessoaController.java
+++ src/main/java/com/mscosta/ctaluguelapi/controller/PessoaController.java
@@ -5,9 +5,7 @@ import com.mscosta.ctaluguelapi.dto.response.PessoaResponseDto;
 import com.mscosta.ctaluguelapi.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/pessoa")
@@ -15,11 +13,22 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
     @PostMapping
-    public ResponseEntity<PessoaResponseDto> inserePessoa(PessoaRequestDto pessoaRequestDto) {
+    public ResponseEntity<PessoaResponseDto> cadastarPessoa(PessoaRequestDto pessoaRequestDto) {
         try {
            final var pessoaResponseDto = pessoaService.createPessoa(pessoaRequestDto);
             return ResponseEntity.ok(pessoaResponseDto);
         }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaResponseDto> getPessoaById(@PathVariable("id") long id) {
+        try {
+            final var response = pessoaService.getPessoaById(id);
+            return new ResponseEntity<>(response, response != null ? org.springframework.http.HttpStatus.OK : org.springframework.http.HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return ResponseEntity.badRequest().build();
